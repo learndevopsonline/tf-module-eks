83a0a20cdf22954e2a8276552d0aa7a56eecfbaa
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-28 18:47:01 +0530

    Adding One more Workflow

--- eks.tf
+++ eks.tf
@@ -3,8 +3,6 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.eks-cluster-role.arn
   version  = var.eks_version
 
-
-
   vpc_config {
     endpoint_private_access = true
     endpoint_public_access  = true
